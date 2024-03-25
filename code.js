62826dcfe69fed58aed1f39bf0f056aaf8942930
MelkMan419 <116685383+MelkMan419@users.noreply.github.com>
2024-03-25 14:35:43 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,17 @@
-function augmentingPath(graph, start, end) {
+function augmentingPath(graph, start, end, visited = {}) {
+    if (start === end) {
+        return [start];
+    }
+    visited[start] = true;
+    for (let neighbor in graph[start]) {
+        if (!visited[neighbor] && graph[start][neighbor] > 0) {
+            let path = augmentingPath(graph, neighbor, end, visited);
+            if (path.length > 0) {
+                return [start, ...path];
+            }
+        }
+    }
     return [];
 }
+
+module.exports=augmentingPath;
